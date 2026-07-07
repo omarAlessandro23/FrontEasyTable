0d21f0c6e55e6867347ea6b1a1d5bf7529a65c3e
omar alessandro collahua huaranga <u20231b381@upc.edu.pe>
2026-07-07 16:21:28 -0500

    Change base URL for development environment

--- FrontEasyTable/src/environments/environment.development.ts
+++ FrontEasyTable/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  base: 'http://localhost:8080',
+  base: 'https://easytable-backend.onrender.com',
   googleMapsKey: 'AIzaSyCJt8y11hGLErMQs7XEoxHurUB-EnMT6s4',
   geminiKey: 'AQ.Ab8RN6L7Z_JXw3IZSEN8OgkifTC0Zn48VyubF-3Gajx_k9rdDw',
 }
