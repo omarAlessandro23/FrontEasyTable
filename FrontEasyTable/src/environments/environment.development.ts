6cbfb13d479f0469dcb782c57c343efb68636da8
omar alessandro collahua huaranga <u20231b381@upc.edu.pe>
2026-07-07 19:33:03 -0500

    Fix formatting of base URL in environment file

--- FrontEasyTable/src/environments/environment.development.ts
+++ FrontEasyTable/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  base: 'https://easytable-backend.onrender.com'
+  base: 'https://easytable-backend.onrender.com',
   googleMapsKey: 'AIzaSyCJt8y11hGLErMQs7XEoxHurUB-EnMT6s4',
   geminiKey: 'AQ.Ab8RN6L7Z_JXw3IZSEN8OgkifTC0Zn48VyubF-3Gajx_k9rdDw',
 }
